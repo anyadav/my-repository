cddc8a6328fd01a64fc20ccd349756c5cf8ffc67
Amar N Yadav <amaryadav.01@gmail.com>
2018-07-01 00:30:02 +0530

    message queue snd rcv program updated

--- unix-system-programming/ipc/messageQ/msgqrcv.c
+++ unix-system-programming/ipc/messageQ/msgqrcv.c
@@ -1,12 +1,21 @@
+//#include<stdlib.h>
+#include<stdio.h>
+//#include<string.h>
+//#include<errno.h>
+//#include<unistd.h>
+
+
+//#include<sys/types.h>
 #include<sys/ipc.h>
 #include<sys/msg.h>
+
 
 /*THIS PROGRAM NEEDS TO BE COMPLTED, JUST COPIED THE SEND*/
 
 struct msg_queue
 {
 
-long mtype;
+long int mtype;
 char name [20];
 int tel;
 };
@@ -16,13 +25,23 @@ int type[20];
 int main()
 {
 
+long int msg_to_rcv = 1;
 struct msg_queue mq;
-int key,msqid,i=0;
+int key,msgid,i=0;
 
 key =ftok(".",'A');
 
-msgget(key,IPC_CREAT|0666);
+msgid = msgget(key,IPC_CREAT|0666);
 
+
+msgrcv(msgid, (void *)&mq, BUFSIZ, msg_to_rcv,0);
+printf("You wrote:  %s\n",mq.name);
+
+msgctl(msgid,IPC_RMID,0);
+
+
+
+/*
 while(1)
 {
 printf("Enter the mtype :");
@@ -36,6 +55,8 @@ scanf("%d\n",&(mq.tel));
 msgsnd(msqid,&mq,sizeof(mq),0);
 printf("\n");
 }
+*/
+
 return 0;
 }
 
--- unix-system-programming/ipc/messageQ/msgqsnd.c
+++ unix-system-programming/ipc/messageQ/msgqsnd.c
@@ -1,14 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
 #include<sys/ipc.h>
 #include<sys/msg.h>
 
 struct msg_queue
 {
 
-long mtype;
+long int mtype;
 char name [20];
 int tel;
 };
 
+char buff[20];
 int type[20];
 
 int main()
@@ -21,6 +25,19 @@ key =ftok(".",'A');
 
 msgget(key,IPC_CREAT|0666);
 
+//Fill data to send
+mq.mtype = 1;
+
+printf("Enter some data to send: ");
+fgets(buff, 20, stdin);
+
+mq.mtype = 1;
+strcpy( mq.name, buff);
+
+//mq.name ="AMAR NATH YADAV";
+mq.tel = 9999;
+
+/*
 while(1)
 {
 printf("Enter the mtype :");
@@ -30,10 +47,11 @@ printf("Enter Name :");
 scanf("%s\n",&(mq.name));
 printf("Enter Tel :");
 scanf("%d\n",&(mq.tel));
+*/
+
 
 msgsnd(msqid,&mq,sizeof(mq),0);
 printf("\n");
-}
 return 0;
 }
 
