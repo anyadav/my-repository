3564c8b1b6d515274a728730436af77fd9be9fc2
AMAR YADAV <amaryadav.01@gmail.com>
2018-07-06 15:39:32 +0530

    Update btree-getheight.c

--- datastructures/tree/btree-getheight.c
+++ datastructures/tree/btree-getheight.c
@@ -1,14 +1,12 @@
-/* VERY BASIC PROGRAM of BINARY TREE */
 #include<stdio.h>
 #include<malloc.h>
 
 
 //Declare node
 struct node{
-
-int data;
-struct node *left;
-struct node *right;
+	int data;
+	struct node *left;
+	struct node *right;
 };
 
 
@@ -20,13 +18,13 @@ struct node *right;
 
 struct node* makenode(int data)
 {
-struct node* node = (struct node*)malloc(sizeof(struct node));
+	struct node* node = (struct node*)malloc(sizeof(struct node));
 
-node->data = data;
-node->left = NULL;
-node->right = NULL;
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
 
-return(node);
+	return(node);
 }
 
 
@@ -34,36 +32,36 @@ return(node);
 
 int getheight(struct node* tree)
 {
-if(tree == NULL)
-{ return 0 ;}
-else{
-int lheight=getheight(tree->left);
-int rheight=getheight(tree->right);
+	if(tree == NULL)
+	{ return 0 ;}
+	else{
+	int lheight=getheight(tree->left);
+	int rheight=getheight(tree->right);
 
-if(lheight > rheight)
-	return (lheight+1);  //here +1 takes care of increaing and ultimately getting the height
-else 
-	return (rheight+1); //here +1 takes care of increaing and ultimately getting the height
+	if(lheight > rheight)
+		return (lheight+1);  //here +1 takes care of increaing and ultimately getting the height
+	else 
+		return (rheight+1); //here +1 takes care of increaing and ultimately getting the height
 
-}
+	}
 }
 
 
 int main()
 {
 
-int height;
-struct node *root = (struct node*)NULL;
+	int height;
+	struct node *root = (struct node*)NULL;
 
-root=makenode(1);
-root->left = makenode(2);
-root->right = makenode(3);
-root->left->left = makenode(4);
+	root=makenode(1);
+	root->left = makenode(2);
+	root->right = makenode(3);
+	root->left->left = makenode(4);
 
-height=getheight(root);
-printf("Height of tree is: %d\n",height);
+	height=getheight(root);
+	printf("Height of tree is: %d\n",height);
 
-return(0);
+	return(0);
 }
 
 
