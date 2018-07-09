8cf0c610ce4f62f71948b178a6e2653b8ee80e5e
AMAR YADAV <amaryadav.01@gmail.com>
2018-07-09 17:34:55 +0530

    Update btree-ops.c

--- datastructures/tree/btree-ops.c
+++ datastructures/tree/btree-ops.c
@@ -15,12 +15,12 @@ struct node *right;
 struct node* makenode(int data);
 int getheight(struct node* tree);
 void traverse(struct node* tree);
-
-
+void printnodeatlevel(struct node* tree,int level);
+struct node* insertinbinarytree(struct node*, int);
 
 int main()
 {
-int height;
+int height,level;
 
 struct node *tree=(struct node*)NULL;
 tree=makenode(10);
@@ -37,15 +37,70 @@ tree->right->right = makenode(25);
 height = getheight(tree);
 printf("Height of tree: %d\n",height);
 
+printf("All the node data of tree:   ");
 traverse(tree);
 printf("\n");
+
+for(level=1;level<=height;level++)
+{
+printf("node data at level %d:  ",level);
+printnodeatlevel(tree,level);
+printf("\n");
+}
+
 
 return 0;
 }
 
+/*   **********UNDER PROGRESS***************
+ *
+ *   Fucntion: insertinbinarytree
+ *   Description: This function takes two parameter a input
+ *   -pointer to root node of tree
+ *   -Data to be inserted(here we are dealing with int data as of now
+ */   
+struct node* insertinbinarytree(struct node* tree, int data)
+{
+struct node* temp =tree;
+if(temp==(struct node*)NULL)
+{
+temp->data=data;
+temp->left=(struct node*)NULL;
+temp->right=(struct node*)NULL;
+
+return tree;
+}
+else if(temp->left == (struct node*)NULL)
+{
+temp->left->data=data;
+temp->left->left=(struct node*)NULL;
+temp->left->right=(struct node*)NULL;
+return tree;
+}
+
+}
 
 
 
+/*The function: printnodeatlevel prints the node data at a perticular level.
+ * Input: take two input:
+ * -pointer to root node of tree
+ * -level, an integer value 
+ *Output: prints all the node data at level provided in input. 
+ * This is basically the breadth first traversal if you print for all the level
+ *  */
+void printnodeatlevel(struct node* tree, int level)
+{
+if(tree == NULL) return;
+else if(level ==1)
+printf("%d  ",tree->data);
+else
+{
+printnodeatlevel(tree->left,level-1);
+printnodeatlevel(tree->right,level-1);
+
+}
+}
 
 //CREATE NODE
 struct node* makenode(int data)
@@ -83,7 +138,7 @@ else
 
 
 
-
+/*TRAVERSE AND PRINT ALL THE NODES DATA OF TREE */
 void traverse(struct node* tree)
 {
 if(tree==(struct node*)NULL) return ;
@@ -92,6 +147,7 @@ else
 printf("%d  ",tree->data);
 traverse(tree->left);
 traverse(tree->right);
+
 }
 }
 
